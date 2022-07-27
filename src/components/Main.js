a3345bb97b97b166e814e8adb78ee1447a0193c9
NewtonOmbese <newtonombese1@gmail.com>
2022-07-27 12:59:29 +0300

    adding my component and my src folder

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Main(){
+    return(
+        <div>
+           <div className="header">
+            <div className="row1">
+                <h1>There's no limit to knowledge gaining <br />Grab a Book Now and Read</h1>
+            </div>
+            <div className="row2">
+                <h2>Find Your Book</h2>
+                <div className="search">
+                    <input type="text" placeholder="Enter The Book's Name" />
+                    <button></button>
+                </div>
+            </div>
+           </div> 
+        </div>
+    )
+}
