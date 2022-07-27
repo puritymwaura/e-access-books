90a2017505ba3329f5175add8e1171eb1687a5e3
NewtonOmbese <newtonombese1@gmail.com>
2022-07-27 15:18:55 +0300

    adding my card component styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ function Card(){
     return(
         <div>
             <div className="card">
-                <img src="./images/book.png" alt=" " />
+                <img src="./images/book.jpg" alt=" " />
                 <div className="bottom">
                     <h3 className="title">React Js</h3>
                     <p className="amount">&#36;2290</p>
