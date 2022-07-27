748e8d8841ee14db99bdb0edb6f3eeebb5c2d8f7
NewtonOmbese <newtonombese1@gmail.com>
2022-07-27 18:34:30 +0300

    working on my main component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,26 @@
 import React from "react";
 
-function Card(){
+function Card({book}){
+    console.log(book)
     return(
         <div>
-            <div className="card">
-                <img src="./images/book.jpg" alt=" " />
-                <div className="bottom">
-                    <h3 className="title">React Js</h3>
-                    <p className="amount">&#36;2290</p>
-                </div>
-            </div>
-        </div>
+        {
+            book.map((item)=>{
+                let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+                return(
+                    <div>
+                        <div className="card">
+                            <img src={thumbnail} alt=" " />
+                            <div className="bottom">
+                                <h3 className="title">React Js</h3>
+                                <p className="amount">&#36;2290</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+    </div>    
     )
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "./Card";
+import axios from "axios";
 
 function Main(){
+    const [search,setSearch]=useState("");
+    const [bookData,setData]=useState([]);
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
     return(
         <div>
-           <div className="header">
-            <div className="row1">
-                <h1>There's no limit to knowledge gaining <br />Grab a Book Now and Read</h1>
-            </div>
-            <div className="row2">
-                <h2>Find Your Book</h2>
-                <div className="search">
-                    <input type="text" placeholder="Enter The Book's Name" />
-                    <button><i class="fa fa-search" aria-hidden="true"></i></button>
+            <div className="header">
+                <div className="row1">
+                    <h1>There's no limit to knowledge gaining <br />Grab a Book Now and Read</h1>
                 </div>
-                <img src="./images/bg2.png" alt=" " />
+                <div className="row2">
+                    <h2>Find Your Book</h2>
+                    <div className="search">
+                        <input type="text" placeholder="Enter Your Book Name"
+                        value={search} onChange={e=>setSearch(e.target.value)}
+                        onKeyPress={searchBook}/>
+                        <button><i className="fas fa-search"></i></button>
+                    </div>
+                    <img src="./images/bg2.png" alt="" />
+                </div>
             </div>
-           </div> 
 
-           <div className="container">
-            <Card />
-           </div>
+            <div className="container">
+              {
+                    <Card book={bookData}/>
+              }  
+            </div>
         </div>
     )
 }
