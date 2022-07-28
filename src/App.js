dc447e05da4cac09c27557c86eae9937e7a90eca
NewtonOmbese <newtonombese1@gmail.com>
2022-07-28 18:16:01 +0300

    getting adequate data for my json file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Main from "./components/Main";
+import NavBar from "./components/NavBar";
 import "./components/style.css";
 import Welcome from "./components/Welcome";
+import { Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
     <div>
-    <Main />
+      <NavBar />
+      <Switch>
+        <Route exact path="/home">
+          <Welcome />
+        </Route>
+        <Route exact path="/books">
+          <Main />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavBar(){
     return(
         <div>
-        
+            <div className="Navbar">
+                <NavLink to="/home">
+                    Home
+                </NavLink>
+                <NavLink to="/books">
+                    Books
+                </NavLink>
+                <p>Learn and be a Master of Educational Art</p>
+            </div>
         </div>
     )
 }
-
 export default NavBar;
